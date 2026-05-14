922c8d82e9c190491346b9da39b35a29d30aaa09
Purush99kr <purushottam9934@gmail.com>
2026-05-14 17:36:49 +0530

    operations completed

---
+++ JavaScript/01_basics/04_operations.js
@@ -0,0 +1,47 @@
+// Operations in JavaScript
+/*
+    1. Arithmetic Operators [+ , - , * , / , % , ** , ++ , --]
+    2. Assignment Operators** [= , += , -= , *= , /= , %= , **=]
+    3. Comparison Operators [== , === , != , !== , > , < , >= , <=]
+    4. Logical Operators [&& , || , !]
+    5. Bitwise Operators [& , | , ^ , ~ , << , >> , >>>]
+    6. String Operators [+ , +=]
+    7.Conditional (Ternary) Operator [condition ? trueValue : falseValue]
+    8. Type Operators [typeof , instanceof]
+    9. Unary Operators [+ , - , ++ , -- , !]
+    10. Relational Operators [in , instanceof]
+    11. Spread Operator [...]
+    12. Rest Operator [...]
+    13. Optional Chaining Operator [?.]
+    14. Nullish Coalescing Operator [??]
+    15. Comma Operator [ , ]
+    16. Delete Operator [delete]
+    17. Void Operator [void]
+    18. New Operator [new]
+*/
+
+// console.log(3 + 5);
+// console.log(3 - 5);
+// console.log(3 * 5);
+// console.log(3 ** 5);
+// console.log(3 / 5);
+// console.log(3 % 5);
+
+let Str1 = "Nikhil";
+let Str2 = " Singh";
+console.log(Str1 + Str2);
+
+//conflict operations
+console.log(3 + "5"); //35, string concatenation
+console.log("5" + 3); //53, string concatenation
+console.log("a" + 3); //a3, string concatenation
+console.log("5" + 3 + 3); //533, string concatenation
+console.log(3 + 3 + "5"); //65, string concatenation
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+// console.log(true + true);
+// console.log(true+) //invalid
+
+//-----------------------------( comparison )-----------------------------
